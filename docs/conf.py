63e24e081eeefefd5b5914fc437f5754e5a549cd
Shipeng Feng <fsp261@gmail.com>
2020-05-10 19:32:17 +0800

    Added github button to docs

--- docs/conf.py
+++ docs/conf.py
@@ -59,6 +59,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'alabaster'
 
+html_theme_options = {
+    'github_button': True,
+    'github_user': 'fengsp',
+    'github_repo': 'django-grpc-framework',
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
