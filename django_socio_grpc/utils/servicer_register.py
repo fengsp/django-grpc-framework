2674d38f33349a058a916cb1e939d8bdb490faff
Adrien Montagu <adrienmontagu@gmail.com>
2021-04-30 18:06:55 +0200

    add AppHandlerRegistry and test it

---
+++ django_socio_grpc/utils/servicer_register.py
@@ -0,0 +1,26 @@
+from importlib import import_module
+
+
+class AppHandlerRegistry:
+    def __init__(self, app_name, server, service_folder="services", grpc_folder="grpc"):
+        self.app_name = app_name
+        self.server = server
+        self.service_folder = service_folder
+        self.grpc_folder = grpc_folder
+
+    def register(self, model_name):
+        pb2_grpc = import_module(
+            f"{self.app_name}.{self.grpc_folder}.{self.app_name}_pb2_grpc"
+        )
+        add_server = getattr(pb2_grpc, f"add_{model_name}ControllerServicer_to_server")
+        if self.service_folder:
+            model_service_path = (
+                f"{self.app_name}.{self.service_folder}.{model_name.lower()}_service"
+            )
+        else:
+            model_service_path = f"{self.app_name}.services"
+        ModelService = getattr(
+            import_module(model_service_path),
+            f"{model_name}Service",
+        )
+        add_server(ModelService.as_servicer(), self.server)
