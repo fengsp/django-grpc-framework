c7c1c64b2dad324f53c42799629fe4b5354931ae
Adrien <adrienmontagu@gmail.com>
2021-05-05 19:35:35 +0200

    working on cleaning servicer

--- django_socio_grpc/services.py
+++ django_socio_grpc/services.py
@@ -8,7 +8,71 @@ from django_socio_grpc.settings import grpc_settings
 
 logger = logging.getLogger("django_socio_grpc")
 
+class Servicer:
 
+    def __init__(self, ServiceClass, **initkwargs):
+        self.service_instance = ServiceClass(**initkwargs)
+        self.grpc_async = os.environ.get("GRPC_ASYNC")
+
+
+    def call_handler(self, action):
+        grpc_async = os.environ.get("GRPC_ASYNC")
+
+        if self.grpc_async:
+
+            async def async_handler(request, context):
+                print("lalalal\n"*6)
+                # db connection state managed similarly to the wsgi handler
+                db.reset_queries()
+                db.close_old_connections()
+                try:
+                    self.service_instance.request = request
+                    self.service_instance.context = GRPCSocioProxyContext(context, action)
+                    self.service_instance.action = action
+                    self.service_instance.before_action()
+                    return await getattr(self.service_instance, action)(self.service_instance.request, self.service_instance)
+                except GRPCException as grpc_error:
+                    logger.error(grpc_error)
+                    context.abort(
+                        grpc_error.status_code, grpc_error.get_full_details()
+                    )
+                finally:
+                    db.close_old_connections()
+
+            return async_handler
+
+        else:
+
+            def handler(request, context):
+                print("iicicic\n"*6)
+                # db connection state managed similarly to the wsgi handler
+                db.reset_queries()
+                # INFO - AM - 22/04/2021 - next line break tests. Need to more understand the drowback about memory in production
+                # db.close_old_connections()
+                try:
+                    self.service_instance.request = request
+                    self.service_instance.context = GRPCSocioProxyContext(context, action)
+                    self.service_instance.action = action
+                    self.service_instance.before_action()
+                    return getattr(self.service_instance, action)(self.service_instance.request, self.service_instance)
+                except GRPCException as grpc_error:
+                    logger.error(grpc_error)
+                    context.abort(
+                        grpc_error.status_code, grpc_error.get_full_details()
+                    )
+                finally:
+                    # INFO - AM - 22/04/2021 - next line break tests. Need to more understand the drowback about memory in production
+                    # db.close_old_connections()
+                    pass
+
+            return handler
+
+    def __getattr__(self, action):
+        print(action)
+        if not hasattr(self.service_instance, action):
+            return not_implemented
+
+        return self.call_handler(action)
 class Service:
 
     authentication_classes = grpc_settings.DEFAULT_AUTHENTICATION_CLASSES
